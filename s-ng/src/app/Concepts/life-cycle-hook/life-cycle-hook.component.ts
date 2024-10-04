3b958ce170704cbdedfd0dbe52fe02ea8422c0b1
Hossain K. M. <awarehossain@gmail.com>
2024-10-05 03:28:04 +0600

    initial commit

--- s-ng/src/app/Concepts/life-cycle-hook/life-cycle-hook.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-life-cycle-hook',
-  templateUrl: './life-cycle-hook.component.html',
-  styleUrl: './life-cycle-hook.component.css',
-})
-export class LifeCycleHookComponent {}
